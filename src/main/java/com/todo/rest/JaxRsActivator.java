cfc8e355c0bb1383a11e55305ff47f929e1c345e
Shekhar Gulati <shekhargulati84@gmail.com>
2013-01-25 09:57:37 +0530

    code added for UI

--- src/main/java/com/todo/rest/JaxRsActivator.java
+++ src/main/java/com/todo/rest/JaxRsActivator.java
@@ -3,7 +3,7 @@ package com.todo.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/rest")
 public class JaxRsActivator extends Application {
    /* class body intentionally left blank */
 }
